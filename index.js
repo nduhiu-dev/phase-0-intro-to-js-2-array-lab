0b5089427087ae3d8e8fe0be32401686c5e7de04
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2025-09-25 20:08:07 +0300

    Completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+// Global array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+// Non-destructive functions
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
